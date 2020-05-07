543ac9e2cd8b5f170f78cfa3d6702714334e022e
He._.na <53573049+harshita2002@users.noreply.github.com>
2020-05-07 21:23:59 +0530

    updated the changes

--- one.java
+++ one.java
@@ -1,9 +1,19 @@
-def func():
-    print("func() ran in one.py")
+/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+ The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.*/
 
-print("top-level print inside of one.py")
 
-if __name__ == "__main__":
-    print("one.py is being run directly")
-else:
-    print("one.py is being imported into another module")
+public class one
+{
+
+public static void main(String[] args)
+{
+// TODO Auto-generated method stub
+System.out.println("The sum of all the multiples of 3 or 5 below 1000");
+int a=(333*(6+332*3))/2; //AP with d=3
+int b=(199*(10+198*5))/2;   //AP with d=5
+int c=(66*(30+65*15))/2; //AP with d=15
+int sum=a+b-c; //final calculation
+System.out.print(sum);
+}
+} 
